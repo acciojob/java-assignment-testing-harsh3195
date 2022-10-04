f1cbb95045fb27ad4940e194399f8593b380c8d1
harsh3195 <harshs.iitd@gmail.com>
2022-10-04 13:41:13 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,19 +1,45 @@
-/**
- * Main class of the Java program. 
- * 
- */
+package com.driver;
+
 import java.util.*;
 
 public class Main {
+
+    public static String func(int N){
+        String s = "";
+        boolean toggle = false;
+        
+        char[] alphabets = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+        
+        Queue<Character> q = new LinkedList();
+        
+        for(int i=0; i<alphabets.length; i++)
+            q.add(alphabets[i]);
+        
+        while(N > 0) {
+            char ch = q.remove();
+            if (toggle)
+                s = s + ch;
+            else
+                s = ch + s;
+            
+            toggle = !toggle;
+            
+            q.add(ch);
+            N = N-1;
+        }
+        
+        return s;
+    }
 
     public static void main(String[] args) {
         //your code here
          Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
+        System.out.println(N);
         String s = "";
         boolean toggle = false;
         
-        char[] alphabets = "abcdefghijklmnopqrstuvwyz".toCharArray();
+        char[] alphabets = "abcdefghijklmnopqrstuvwxyz".toCharArray();
         
         Queue<Character> q = new LinkedList();
         
@@ -34,5 +60,5 @@ public class Main {
         }
         System.out.println(s);
 
-           }
+    }
 }
